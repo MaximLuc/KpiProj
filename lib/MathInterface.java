d21da6615c0446aba6db4ecbd239f1c7a4ad8381
Максим Патратий <maksimpatratij@MacBook-Air-Maksim.local>
2023-09-12 21:14:06 +0300

    2 commit

--- lib/MathInterface.java
+++ lib/MathInterface.java
@@ -4,4 +4,5 @@ public interface MathInterface {
     
     long myFactorial(int n);
     double myPower(double a,int n);
+    int SumBy(int a,int b);
 }
--- lib/MyMath.java
+++ lib/MyMath.java
@@ -20,6 +20,15 @@ public class MyMath implements MathInterface{
         }
         return p;
     }
+
+    @Override
+    public int SumBy(int a, int b) {
+        int sum= 0;
+        for (int i = a; i <= b; i++) {
+            sum+=i;
+        }
+        return sum;
+    }
     
 
     
